43e3620055156e7a78b672133dd1bcf56026bba1
jyoti <ping2jyoti786@gmail.com>
2019-11-28 18:54:20 +0530

    add forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (latitude, longtitude, callback) => {
 
             const temperature = body.currently.temperature;
             const precipProbability = body.currently.precipProbability;
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + temperature + ' degrees out. There is a ' + precipProbability + '% chance of rain.');
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + temperature + ' degrees out. There is a ' + precipProbability + '% chance of rain. use this site to check weather in your area.');
         }
     })
 
